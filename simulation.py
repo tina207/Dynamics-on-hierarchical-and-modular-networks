22565f3087ba4b944ee61380d0824c183eba9a1c
Victor <vbuendiar@gmail.com>
2022-08-02 13:58:40 +0200

    Added angles and Kuramoto order parameter for the system

--- simulation.py
+++ simulation.py
@@ -9,7 +9,10 @@ dt = 0.01
 sqdt = np.sqrt(dt)
 t0 = 0
 t_end = 60*5
+t_relax = t_end
+
 t_steps = int((t_end - t0) / dt)
+t_relax_steps = int((t_relax - t0) / dt)
 
 Delta = 0.5
 sigma = 0.5
@@ -27,17 +30,56 @@ n_length = len(neighbors_vector)
 def do_simulation(network, omega, a, J, J_int, x=np.zeros((n_length, t_steps)), y=np.zeros((n_length, t_steps))):
     N = len(network)
 
-
-    if np.all(x) == 0:
-        # random initial values if there is none given
-        x[:, 0] = np.random.rand(N) * 0.5
-        y[:, 0] = np.random.rand(N) * 0.5
+    xr = np.random.rand(N)
+    yr = np.random.rand(N)
 
     # Radius of polar coordinates
     R = np.zeros(t_steps)
 
+    for k in range(1, t_relax_steps):
+        x_old = xr
+        y_old = yr
+
+        for i in range(N):
+            coup_x = 0
+            coup_y = 0
+
+            for neigh in network[i]:
+                coup_x += x_old[neigh]
+                coup_y += y_old[neigh]
+
+            # variables
+            n_neighs = len(network[i])
+            x_sq = x_old[i] * x_old[i]
+            y_sq = y_old[i] * y_old[i]
+            term = J_int * (1 - x_sq - y_sq)
+
+            # noise
+            r_gaussian = np.random.standard_normal(2)
+
+            # neural mass models
+            xr[i] = ((0.5 * (
+                    -2 * omega * y_old[i] - x_old[i] * (Delta_sq - term) - a * (1 - x_sq + y_sq))) + J * (
+                               coup_x - x_old[i] * n_neighs) / n_neighs) * dt + sigma * r_gaussian[0] * sqdt + x_old[
+                          i]
+            yr[i] = ((0.5 * (2 * omega * x_old[i] - y_old[i] * (Delta_sq - term) + 2 * a * x_old[i] * y_old[
+                i])) + J * (coup_y - y_old[i] * n_neighs) / n_neighs) * dt + sigma * r_gaussian[1] * sqdt + y_old[
+                          i]
+
+    # random initial values if there is none given
+    x[:, 0] = xr
+    y[:, 0] = yr 
+
+    #Initialize average Z
+    z = xr + 1.0j * yr
+    z = z.mean() 
+    R[0] = abs(z)
+
+    #TODO!!! Initialize average Kuramoto
+
     for k in range(1, t_steps):
         z = 0
+        kuramoto =0.0
 
         for i in range(N):
             coup_x = 0
@@ -67,9 +109,15 @@ def do_simulation(network, omega, a, J, J_int, x=np.zeros((n_length, t_steps)), 
 
             z += complex(x[i, k], y[i, k])
 
-        z /= N
-        R[k] = abs(z)
+            psi = np.arctan2(y[i,k], x[i,k])
+            kuramoto += np.exp(1.0j * psi)
 
+        z /= N
+        kuramoto /= N
+
+        #TODO!!!!
+        #R[k] = abs(z)
+        R[k] = abs(kuramoto)
     return x, y, R
 
 
@@ -85,5 +133,17 @@ if __name__ == "__main__":
         neighbors.append(element)
 
 
-    x_new, y_new, r = do_simulation(network=neighbors, omega=1, a=0, J=0, J_int=1)
+    x_new, y_new, r = do_simulation(network=neighbors, omega=1, a=0.95, J=0, J_int=1)
 
+"""
+    plt.figure()
+    for j in range(5):
+        plt.plot(np.arange(y_new[0,:].size), x_new[j, :]*x_new[j, :] +  y_new[j, :]*y_new[j, :] )
+    plt.show()
+
+    plt.figure()
+    plt.plot(r)
+    plt.show()
+
+    print(r.mean())
+"""
